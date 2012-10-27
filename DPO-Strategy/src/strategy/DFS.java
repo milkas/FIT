f87e10553e21ddd3738a1d82e75390e05cb749a2
milkas <hrstka.miroslav@gmail.com>
2012-10-27 17:50:51 +0200

    first commit

---
+++ DPO-Strategy/src/strategy/DFS.java
@@ -0,0 +1,22 @@
+package strategy;
+
+/**
+ *
+ * @author Miroslav Hrstka
+ */
+public class DFS implements Strategy {
+    int[] pole;
+    
+    public DFS(int[] pole){
+        this.pole=pole;
+    }
+
+    @Override
+    public void vypis(int it) {
+        if (it < pole.length) {
+            System.out.print(pole[it] + " ");
+            vypis(2*it);
+            vypis(2*it + 1);
+        }
+    }
+}
